82a83483d3aa134ef85186cefb9642ad9b188d6d
Ari Badovska <160342977+AriSnowie97@users.noreply.github.com>
2025-03-12 09:06:32 +0200

    Update PythonApplication2.py

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -1,3 +1,4 @@
+print("Task 1")
 def creatingandcalculating(text):
     try:
         number = int(text)
@@ -10,6 +11,28 @@ def creatingandcalculating(text):
             return "Invalid input: This text is not number"
     result_text = "The result is: " + str(result)
     return result_text
+
 text = "123"
 result = creatingandcalculating(text)
+print(result)
+
+print("Task 2")
+def arifmetic_operation(text1, text2):
+    try:
+        number1 = float(text1)
+        number2 = float(text2)
+        sum = number1 + number2
+        difference = number1 - number2
+        multiplication = number1 * number2
+        division = number1 / number2
+        result_text = "Sum: " + str(sum) + ", Difference: " + str(difference) + ", Multiplication: " + str(multiplication) + ", Division: " + str(division)
+    except ValueError:
+        return "Invalid input: This text is not number"
+    except ZeroDivisionError:
+        return "Division by zero"
+    return result_text
+
+text_number1 = "145"
+text_number2 = "12"
+result = arifmetic_operation(text_number1, text_number2)
 print(result)
